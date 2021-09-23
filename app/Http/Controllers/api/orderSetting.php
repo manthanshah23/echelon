d3e9ab538a77494e339e87bf835e90b209ad92c4
Manthan <mackshah1994@gmail.com>
2021-09-23 14:39:35 +0530

    initial

--- app/Http/Controllers/api/orderSetting.php
+++ app/Http/Controllers/api/orderSetting.php
@@ -5,11 +5,34 @@ namespace App\Http\Controllers\api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use GuzzleHttp\Client;
-use App\Http\Controllers\api\ApiController;
 
 
 class orderSetting extends Controller
 {
+    public static function GetApi($url)
+    {
+        $client = new Client();
+        $request = $client->get($url,[
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ]
+        ]);
+        $response = json_decode($request->getBody(), true);
+        return $response;
+    }
+
+    public static function PostApi($url,$body,$type="application/graphql") {
+        $client = new Client();
+        $request = $client->request('post', $url, [
+            'headers' => [
+                'Content-Type' => $type
+            ],
+            'body' => $body
+        ]);
+        $response = json_decode($request->getBody(), true);
+        return $response;
+    }
+
 
     /**
      * Display a listing of the resource.
@@ -18,21 +41,23 @@ class orderSetting extends Controller
      */
     public function index()
     {
-       $api = new ApiController;
         $response = ["response"=>"Data not found"];
-        $att_url="graphql.json";
-        $url =$api->base_url($att_url);
         $data_json = file_get_contents('php://input');
         $data_arr = json_decode($data_json, TRUE);
-        $dates=$api->date_format($data_arr['DateTime']);
-        $body = "{\r\n  orders(first:100,query:\"created_at:>$dates\"){\r\n    edges{\r\n      node{\r\n        id\r\n      }\r\n    }\r\n  }\r\n}\r\n";
-        $order_api_res =$api->CheckAuth($url,$body,null,'post');
+
+        $username = $data_arr['shopifyConnection']['apiKey'];
+        $password = $data_arr['shopifyConnection']['apiPassword'];
+        $surl = $data_arr['shopifyConnection']['apiUrl'];
+
+        $url = 'https://'.$username.':'.$password.'@'.$surl.'/admin/api/2021-07/graphql.json';
+
+        $body = "{\r\n  orders(first:100,query:\"created_at:>2021-08-18T01:00:00\"){\r\n    edges{\r\n      node{\r\n        id\r\n      }\r\n    }\r\n  }\r\n}\r\n";
+        $order_api_res = $this->PostApi($url,$body);
         if(!empty($order_api_res["data"]['orders']['edges'])){
             $response = array();
             foreach ($order_api_res["data"]['orders']['edges'] as $edge) {
                 $id = explode("/",$edge['node']['id']);
-                $att_url="orders/".end($id).".json";
-                $orderUrl =$api->base_url($att_url);
+                $orderUrl = "https://bdmas.myshopify.com/admin/api/2021-07/orders/".end($id).".json";
                 $orderResponse = $this->GetApi($orderUrl);
                 if(!empty($orderResponse['order'])){
                     array_push($response, $orderResponse['order']);
@@ -46,53 +71,59 @@ class orderSetting extends Controller
 
     public function getSku()
     {
-       $api = new ApiController;
         $response = ["response"=>"Data not found"];
         $data_json = file_get_contents('php://input');
         $data_arr = json_decode($data_json, TRUE);
-        $dates=$api->date_format($data_arr['DateTime']);
-        $att_url="graphql.json";
-        $url =$api->base_url($att_url);
+        $dates = date("Y-m-d", strtotime($data_arr['DateTime']));
+        $dates .= "T";
+        $dates .= date("h:m:s", strtotime($data_arr['DateTime']));
+
+        $username = $data_arr['shopifyConnection']['apiKey'];
+        $password = $data_arr['shopifyConnection']['apiPassword'];
+        $surl = $data_arr['shopifyConnection']['apiUrl'];
+
+        $url = 'https://'.$username.':'.$password.'@'.$surl.'/admin/api/2021-07/graphql.json';
         $body = "{\r\n  products(first:250, query:\"updated_at:>$dates\"){\r\n    edges{\r\n     node{\r\n      id\r\n    }\r\n    }\r\n  }\r\n}\r\n";
-        $order_sku_res = $this->CheckAuth($url,$body,null,"post");
+        $order_sku_res = $this->PostApi($url,$body);
         if(!empty($order_sku_res["data"]['products']['edges'])){
             $ids = array();
             foreach ($order_sku_res["data"]['products']['edges'] as $edge) {
                 $id_explode = explode("/",$edge['node']['id']);
                 array_push($ids, end($id_explode));
             }
-            $att_url = "products.json?ids=".implode(",",$ids);
-            $productGetUrl=$api->base_url($att_url);
-            $response = $this->CheckAuth($productGetUrl,null,null,"get");
+            $productGetUrl = 'https://'.$username.':'.$password.'@'.$surl.'/admin/api/2021-07/products.json?ids='.implode(",",$ids);
+            $response = $this->GetApi($productGetUrl);
         }
         return $response;
     }
 
     public function stockUpdate()
     {
-         $api = new ApiController;
         $response = ["response"=>"Data not found"];
         $data_json = file_get_contents('php://input');
         $data_arr = json_decode($data_json, TRUE);
         $data_arr = $data_arr["inventoryLevel"];
-        $att_url = "products/".$data_arr['shopify_product_id']."/variants/".$data_arr['shopify_variant_id'].".json";
-        $url=$api->base_url($att_url);
-        $variants_res = $this->CheckAuth($url,null,null,"get");
+
+        $username = $data_arr['shopifyConnection']['apiKey'];
+        $password = $data_arr['shopifyConnection']['apiPassword'];
+        $surl = $data_arr['shopifyConnection']['apiUrl'];
+
+        $url = 'https://'.$username.':'.$password.'@'.$surl.'/admin/api/2021-07/products/'.$data_arr['shopify_product_id'].'/variants/'.$data_arr['shopify_variant_id'].'.json';
+
+        $variants_res = $this->GetApi($url);
+
         if(!empty($variants_res["variant"])){
             $inventory_item_id = $variants_res['variant']['inventory_item_id'];
-            $att_url= "inventory_levels.json?inventory_item_ids=".$inventory_item_id;
-            $locationUrl=$api->base_url($att_url);
-
-            $location_res = $this->CheckAuth($locationUrl,null,null,"get");
+            $locationUrl = 'https://'.$username.':'.$password.'@'.$surl.'/admin/api/2021-07/inventory_levels.json?inventory_item_ids='.$inventory_item_id;
+            $location_res = $this->GetApi($locationUrl);
             if(!empty($location_res["inventory_levels"])){
                 $inventory_levels = $location_res['inventory_levels'][0];
                 $location_id = $inventory_levels['location_id'];
                 $inventory_item_id = $inventory_levels['inventory_item_id'];
                 $qty = $data_arr['quantity'];
-                $att_url = "inventory_levels/set.json";
-                $stockUpdateUrl=$api->base_url($att_url);
+                $stockUpdateUrl = 'https://'.$username.':'.$password.'@'.$surl.'/admin/api/2021-07/inventory_levels/set.json';
                 $body = "{\r\n  \"location_id\": ".$location_id.",\r\n  \"inventory_item_id\": ".$inventory_item_id.",\r\n  \"available\": ".$qty."\r\n}";
-                $response = $this->CheckAuth($stockUpdateUrl,$body,"application/json","post");
+                $response = $this->PostApi($stockUpdateUrl,$body,"application/json");
             }
         }
         return $response;
